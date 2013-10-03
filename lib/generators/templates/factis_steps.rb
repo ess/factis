5c701fef77b8a163353b14f615fcf053fa8cdc97
Ess <pooster@gmail.com>
2013-10-03 02:34:08 -0400

    Forgot to change the recall step

--- lib/generators/templates/factis_steps.rb
+++ lib/generators/templates/factis_steps.rb
@@ -2,7 +2,7 @@ Given %r{^I remember (.*?) is "(.*?)"$} do |fact, content|
   remember_fact(fact, content)
 end
 
-Then %r{^I should recall (.*?) as "(.*?)"$} do |expected_content|
+Then %r{^I should recall (.*?) as "(.*?)"$} do |fact, expected_content|
   @remembered_fact.should == expected_content
 end
 
